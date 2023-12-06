00d03c1d1563006b2777f3add6c008f2ebc46c57
Yusuke Morita <ymorita@pixel9.com>
2023-12-06 14:22:15 -0600

    error handling

--- app/root.tsx
+++ app/root.tsx
@@ -1,9 +1,40 @@
 import React from "react";
 import type { LinksFunction } from "@remix-run/node";
-import { Links, Meta, Outlet, Scripts, LiveReload } from "@remix-run/react";
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  LiveReload,
+  useRouteError,
+  isRouteErrorResponse,
+} from "@remix-run/react";
 import styles from "./tailwind.css";
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+  return (
+    <html>
+      <head>
+        <title>Oops!</title>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <h1>
+          {isRouteErrorResponse(error)
+            ? `${error.status} ${error.statusText}`
+            : error instanceof Error
+            ? error.message
+            : "Unknown Error"}
+        </h1>
+        <Scripts />
+      </body>
+    </html>
+  );
+}
 
 export default function App() {
   return (
--- app/service.ts
+++ app/service.ts
@@ -1,14 +1,23 @@
-function apiRequest(path, params) {
+async function apiRequest(path, params) {
   const url = `https://api.yelp.com/v3/businesses/${path}?${new URLSearchParams(
     params
   )}`;
 
-  return fetch(url, {
+  const results = await fetch(url, {
     headers: {
       Authorization: `Bearer ${process.env.YELP_TOKEN}`,
       Accept: "application/json",
     },
   }).then((res) => res.json());
+
+  //   console.log({ results });
+
+  if (results.error) {
+    console.error(results.error.code);
+    throw new Error(results.error.code);
+  }
+
+  return results;
 }
 
 export async function findBoba(location, sort_by, limit = 5) {
