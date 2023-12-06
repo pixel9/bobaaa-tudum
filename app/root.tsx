111920f4553e03f83483a88c205c219f9b86591b
Yusuke Morita <ymorita@pixel9.com>
2023-12-06 10:50:41 -0600

    cleanup imports

--- app/root.tsx
+++ app/root.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import type { LinksFunction } from "@remix-run/node";
-import { Links, Meta, Outlet, Scripts } from "@remix-run/react";
-import { LiveReload } from "@remix-run/react";
+import { Links, Meta, Outlet, Scripts, LiveReload } from "@remix-run/react";
 import styles from "./tailwind.css";
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
